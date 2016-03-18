6a3fd12f473ff4d40a72fea444a8df1d78482a3b
char-lie <webmaster976@gmail.com>
2016-03-18 14:36:15 +0200

    Updated application
    Added command-line argument for Cassandra DB hostname

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,8 +25,12 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            cluster = Cluster.builder().addContactPoints(InetAddress.getLocalHost()).build();
-            //cluster = Cluster.builder().addContactPoints("::ffff:0.0.0.0").build();
+            if (args.length == 0) {
+                cluster = Cluster.builder().addContactPoints(InetAddress.getLocalHost()).build();
+                //cluster = Cluster.builder().addContactPoints("::ffff:0.0.0.0").build();
+            } else {
+                cluster = Cluster.builder().addContactPoints(args[0]).build();
+            }
             session = cluster.connect("mykeyspace");
             CassandraOperations cassandraOps = new CassandraTemplate(session);
             cassandraOps.insert(new Person("1234567890", "David", 40));
