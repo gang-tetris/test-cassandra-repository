d054b5060ddf309500b8ff48a4d4e04facedf604
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-05-08 21:21:06 +0300

    Added script to run Docker container for development

---
+++ docker.sh
@@ -0,0 +1,14 @@
+# Script to run Docker image
+# Assuming that
+#   you have running Cassandra image with name "cassandra"
+#   you have running RabbitMQ image with name "rabbit"
+
+# This script will copy your curreent directory with source code
+# to /src/ folder inside of Docker with Java 8 and Maven 3
+
+# Code can be compiled and run via rerun.sh script inside of Docker container
+
+docker run --rm -it -v $(pwd):/src/ \
+           --link cassandra:cassandra --link rabbit:rabbit \
+           -it maven:3 sh -c "cd /src/ && bash"
+
