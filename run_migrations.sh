4b7542fc802ea5f22363409997c21698ccdcd011
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-08 21:40:26 +0300

    Added script to run Cassandra migrations

---
+++ run_migrations.sh
@@ -0,0 +1,10 @@
+# Script to run Cassandra migrations for docker container with Cassandra
+
+# Receives optional positional argument with Cassandra container name,
+# "cassandra" by default
+
+CASSANDRA_CONTAINER="${1:-cassandra}"
+SCRIPT="cqlsh -f /tmp/migrations.cql || rm /tmp/migrations.cql"
+docker cp migrations.cql ${CASSANDRA_CONTAINER}:/tmp/
+docker exec -it ${CASSANDRA_CONTAINER} sh -c "${SCRIPT}"
+
