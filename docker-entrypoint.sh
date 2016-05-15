fcadbec4166cc0ae42cfafcb53770b9915a6fe55
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-15 17:56:30 +0300

    Updated entrypoint
    Checking Hazelcast running

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,11 @@
 
 until netcat -z -w 2 rabbit 5672; do sleep 1; done
 
-#HAZELCAST_URL="http://0.0.0.0:5701/hazelcast/rest/management/cluster/state"
-#until curl --data "app1&app1-pass" "${HAZELCAST_URL}"
-#do
-#    sleep 1
-#done
+HAZELCAST_URL="http://hazelcast:5701/hazelcast/rest/management/cluster/state"
+until curl --data "app1&app1-pass" "${HAZELCAST_URL}"
+do
+    sleep 1
+done
 
 until wget --spider "cassandra:9042"; do sleep 1; done
 
