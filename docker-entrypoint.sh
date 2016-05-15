6dbbfbd6cf7be45a6fbada8e0239e99cd023bef5
Valeriy Krygin <valeriy.krygin@gmail.com>
2016-05-15 16:42:34 +0300

    Fixed Docker entrypoint
    Not checking Hazelcast
    
    Running Cassandra migrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,16 +5,18 @@ do
     sleep 1
 done
 
-HAZELCAST_URL="http://0.0.0.0:5701/hazelcast/rest/management/cluster/state"
-until curl --data "app1&app1-pass" "${HAZELCAST_URL}"
-do
-    sleep 1
-done
+#HAZELCAST_URL="http://0.0.0.0:5701/hazelcast/rest/management/cluster/state"
+#until curl --data "app1&app1-pass" "${HAZELCAST_URL}"
+#do
+#    sleep 1
+#done
 
 until docker exec cassandra cqlsh -e quit
 do
     sleep 1
 done
+
+exec run_migrations.sh
 
 mvn exec:java -Dexec.mainClass='org.example.app.Main' -Dexec.args="cassandra rabbit"
 
